ec249b88a3f03ffd5d587efaa6209c08d4ed9452
lolodino77 <laurentdangvu@gmail.com>
2020-05-01 10:44:00 +0200

    debug en cours

--- cg.c
+++ cg.c
@@ -23,6 +23,8 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>		/* chronometrage */
+#include <string.h>		/* pour memset */
 #include <err.h>
 #include <math.h>
 #include <getopt.h>
