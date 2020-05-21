9620c13e48a027204e58464caa3e1471d2f60727
DANG-VU <3800623@ppti-14-406-11.ufr-info-p6.jussieu.fr>
2020-05-21 15:04:36 +0200

    debut des tests debugage MPI

--- seq/runner.py
+++ seq/runner.py
@@ -18,7 +18,7 @@ params['proof'] = 0x65dec1542f679f51
 
 # Paramètres du calcul (à adapter)
 params['matrix'] = "bcsstk13"
-params['users'] = ["Charles Bouillaguet", "Lilia Ziane Khodja"]
+params['users'] = ["3800623"]
 
 # Description du code exécuté
 params['software'] = """Code séquentiel fourni.
@@ -36,8 +36,8 @@ Machines de bureau DELL équipés de CPU Intel i3 à 2 coeurs, 3Ghz, et 4Go de R
 #   {cores}  sera remplacé par la valeur ci-dessus.
 #   {seed}   sera remplacé par la valeur fournie par le serveur.
 #   On peut ajouter toutes les options qu'on veut, utiliser mpiexec, etc.
-command_line = "./cg --matrix {matrix}.mtx --seed {seed}"
-#command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
+#command_line = "./cg --matrix {matrix}.mtx --seed {seed}"
+command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | ./cg --seed {seed}"
 #command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix {matrix}.mtx --seed {seed}"
 #command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
 
