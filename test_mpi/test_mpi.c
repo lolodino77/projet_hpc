789739cc38b5adc2c4c569a3805c61fa9f46f9ef
lolodino77 <laurentdangvu@gmail.com>
2020-05-21 16:10:13 +0200

    1

--- test_mpi/test_mpi.c
+++ test_mpi/test_mpi.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
+    int n = 0;
+
     // Initialize the MPI environment
     MPI_Init(NULL, NULL);
 
@@ -10,8 +12,8 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
     // Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+    int my_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     // Get the name of the processor
     char processor_name[MPI_MAX_PROCESSOR_NAME];
@@ -20,8 +22,14 @@ int main(int argc, char** argv) {
 
     // Print off a hello world message
     printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
+           processor_name, my_rank, world_size);
 
+    if(my_rank == 0){
+        n = 10000;
+    }
+    MPI_Bcast(n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+    printf("apres broadcast : n = ", n);
     // Finalize the MPI environment.
     MPI_Finalize();
 }
