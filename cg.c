91a489bfd5378018630ba4cd909d63604a637fda
lolodino77 <laurentdangvu@gmail.com>
2020-05-22 09:09:35 +0200

    1

--- cg.c
+++ cg.c
@@ -401,10 +401,6 @@ int main(int argc, char **argv)
     MPI_Bcast(&nnz, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	else{
 		struct csr_matrix_t *A = malloc(sizeof(*A));
-		int A->Ap = malloc((n + 1) * sizeof(*Ap));
-		int A->Aj = malloc(2 * nnz * sizeof(*Ap));
-		double A->Ax = malloc(2 * nnz * sizeof(*Ax));
-
 		A->n = n;
 		A->Ap = malloc((n+1)*sizeof(int));
 		A->Aj = malloc(2 * nnz*sizeof(int));
