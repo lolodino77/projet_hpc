164f690bd2297cd0136c959d1aeee337fe951593
lolodino77 <laurentdangvu@gmail.com>
2020-05-28 23:16:27 +0200

    version sans equilibrage

--- version_sans_equilibrage/runner.py
+++ version_sans_equilibrage/runner.py
@@ -20,7 +20,7 @@ params['proof'] = 0x65dec1542f679f51
 #bcsstk13
 #cfd2
 #cfd1
-params['matrix'] = "cfd2"
+params['matrix'] = "bcsstk13"
 params['users'] = ["3800623"]
 
 # Description du code exécuté
@@ -43,7 +43,7 @@ Machines de bureau DELL équipés de CPU Intel i3 à 2 coeurs, 3Ghz, et 4Go de R
 # command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | ./cg --seed {seed}"
 # command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz --seed {seed}"
 #command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
-command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/thermal2.mtx.gz | mpiexec --n 5 -hostfile hostfile --map-by ppr:1:node ./cg --seed 45"
+command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | mpiexec --n 16 -hostfile hostfile --map-by ppr:1:node ./cg --seed 45"
 
 ######################### Main Program ###########################
 
