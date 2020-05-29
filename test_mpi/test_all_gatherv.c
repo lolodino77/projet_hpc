3b37a960bd03ae38fe1b1bf9ef6e99bfd21ea17f
lolodino77 <laurentdangvu@gmail.com>
2020-05-29 11:31:20 +0200

    version sans equilibrage

--- test_mpi/test_all_gatherv.c
+++ test_mpi/test_all_gatherv.c
@@ -48,10 +48,10 @@ int main(int argc, char** argv) {
         recvcounts[i] = sendcount;
     }
 
-    int displs[p];
+    int displs[p - 1];
     displs[0] = 0;
     for(int i = 0;i<(p - 2);i++){
-        displs[i + 2] = (i * sendcount) + 2;
+        displs[i + 1] = (i * sendcount) + 2;
     }
     printf("displs : \n");
     for(int i = 0; i < p; i ++){
