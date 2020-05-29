0f9b869a31683d3bebcaf8edc7fbac7cdcf7c9bd
lolodino77 <laurentdangvu@gmail.com>
2020-05-29 17:43:58 +0200

    version sans equilibrage

--- test_mpi/test_all_gatherv.c
+++ test_mpi/test_all_gatherv.c
@@ -4,6 +4,93 @@
 //mpicc -o test_mpi test_mpi.c 
 //mpirun -n 5 -hostfile hostfile --map-by node ./test_mpi
 
+
+//Le gros tableau est de taille 27.
+//Il y a un petit tableau de taille 2. Tous les autres sont de taille 5.
+//On essaye de récupérer ces petits tableaux dans le grand tableau.
+
+
+// /* Petit tableau contenant le reste se trouve au début. */
+// int main(int argc, char** argv){
+//     MPI_Init(NULL, NULL);
+
+//     // Get the number of processes
+//     int p;
+//     MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+//     printf("size = %d\n", p);
+
+//     // Get the rank of the process
+//     int my_rank;
+//     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+//     // Get the name of the processor
+//     char processor_name[MPI_MAX_PROCESSOR_NAME];
+//     int name_len;
+//     MPI_Get_processor_name(processor_name, &name_len);
+
+//     // Print off a hello world message
+//     printf("Hello world from processor %s, rank %d out of %d processors\n",
+//            processor_name, my_rank, p);
+
+//     int n = 12; //taille du grand tableau A
+//     int quotient = n / p;
+//     int reste = n % p; 
+//     int n_part = quotient; //taille du sous-tableau A_part du processeur my_rank
+//     if(my_rank == 0){
+//         n_part = reste;
+//     }    
+//     recvcounts[p-1] = reste;   
+//     printf("%d\n", recvcounts[p-1]);
+
+//     int displs[p - 1]; //taille des premières adresses de chaque petit tableau, dans l'ordre croissant des my_rank
+//     displs[0] = 0;
+//     displs[1] = reste;
+//     for(int i = 2;i < p;i++){
+//         displs[i] = (i - 1) * quotient + reste; // displs[2] = displs[i] = 7 = 1*5 + 2 = (i-1)*5 + 2
+//     }
+//     printf("displs : \n");
+//     for(int i = 0; i < (p - 1); i ++){
+//         printf("%d ", displs[i]);
+//     }
+//     printf("\n");
+
+//     printf("debut gather\n");        
+//     // MPI_Gatherv(A_part, n_part, MPI_INT, A, recvcounts, displs, MPI_INT, 0, MPI_COMM_WORLD)
+//     MPI_Allgatherv(A_part, n_part, MPI_INT, A, recvcounts, displs, MPI_INT, MPI_COMM_WORLD);    
+//     printf("fin gather\n");
+
+//     printf("my_rank = %d\n", my_rank);
+//     if (my_rank == 0)
+//     {
+//         printf("n = %d\n", n);
+//         for(int i = 0;i < n;i++){
+//             printf("%d, ", A[i]);
+//         }
+//     }
+//     printf("\n"); 
+
+//     MPI_Finalize();
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Petit tableau contenant le reste se trouve au début. */
 int main(int argc, char** argv) {
     MPI_Init(NULL, NULL);
 
@@ -57,14 +144,15 @@ int main(int argc, char** argv) {
         printf("%d\n", recvcounts[i]);
     }
 
-    int displs[p - 1];
+    //p = 6
+    int displs[p]; //displs[5]
     displs[0] = 0;
     displs[1] = reste;
     for(int i = 2;i < p;i++){
         displs[i] = (i - 1) * quotient + reste; // displs[2] = displs[i] = 7 = 1*5 + 2 = (i-1)*5 + 2
     }
     printf("displs : \n");
-    for(int i = 0; i < (p - 1); i ++){
+    for(int i = 0; i < p; i ++){
         printf("%d ", displs[i]);
     }
     printf("\n");
