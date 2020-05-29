3323375a0d62ca5ff8b8483e66d162028f51f5cc
lolodino77 <laurentdangvu@gmail.com>
2020-05-29 16:59:50 +0200

    version sans equilibrage

--- test_mpi/test_all_gatherv.c
+++ test_mpi/test_all_gatherv.c
@@ -34,6 +34,7 @@ int main(int argc, char** argv) {
         n_part = reste;
     }
     int n = (p - 1) * n_part + 2;
+    printf("n = %d\n", n);
 
     int* A = malloc(((p - 1) * quotient + reste) * sizeof(int)); // p * n_part = 4 * 5 = 20
     int* A_part = malloc(n_part * sizeof(int)); 
