3867a3292cdb189211588a00cc92e2f359a6d34a
DANG-VU <3800623@ppti-14-406-12.ufr-info-p6.jussieu.fr>
2020-05-31 15:48:31 +0200

    1

--- version_sans_equilibrage/runner.py
+++ version_sans_equilibrage/runner.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-# coding: utf-8
 import array
 import subprocess
 import json
@@ -10,23 +9,38 @@ import base64
 
 #################################################################
 
+# Paramètres du système (ne pas modifier)
 SERVER = "https://hpc.fil.cool"
 params = {}
 params['version'] = 3
 params['proof'] = 0x65dec1542f679f51
 
+# Paramètres du calcul (à adapter)
 params['matrix'] = "bcsstk13"
 params['users'] = ["3800623"]
 
+# Description du code exécuté
 params['software'] = """Code séquentiel fourni.
 Pas d'améliorations apportées."""
 
-params['nodes'] = 4  
-params['cores'] = 8   
+# Description du matériel utilisé pour l'exécution
+params['nodes'] = 4   # nombre de noeuds
+params['cores'] = 8   # nombre total de coeurs
 params['hardware'] = """4 PCs de la Salle 327 (Esclangon).
 Machines de bureau DELL équipés de CPU Intel i3 à 2 coeurs, 3Ghz, et 4Go de RAM."""
 
-command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | mpiexec --n 17 -hostfile hostfile --map-by ppr:1:node ./cg --seed {seed}"
+# Comment exécuter le solveur :
+#   {matrix} sera remplacé par la valeur ci-dessus.
+#   {nodes}  sera remplacé par la valeur ci-dessus.
+#   {cores}  sera remplacé par la valeur ci-dessus.
+#   {seed}   sera remplacé par la valeur fournie par le serveur.
+#   On peut ajouter toutes les options qu'on veut, utiliser mpiexec, etc.
+# command_line = "./cg --matrix {matrix}.mtx --seed {seed}"
+#command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | ./cg --seed 46"
+commandline = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | mpiexec --n 17 -hostfile hostfile --map-by ppr:1:node ./cg --seed 45"
+# command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
+#command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix {matrix}.mtx --seed {seed}"
+#command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
 
 ######################### Main Program ###########################
 
