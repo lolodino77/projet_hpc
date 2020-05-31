3b4dbe2aba4cd851e331073a00547d1877277329
lolodino77 <laurentdangvu@gmail.com>
2020-05-31 15:44:56 +0200

    1

--- seq2/runner.py
+++ seq2/runner.py
@@ -36,7 +36,8 @@ Machines de bureau DELL équipés de CPU Intel i3 à 2 coeurs, 3Ghz, et 4Go de R
 #   {seed}   sera remplacé par la valeur fournie par le serveur.
 #   On peut ajouter toutes les options qu'on veut, utiliser mpiexec, etc.
 # command_line = "./cg --matrix {matrix}.mtx --seed {seed}"
-command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
+command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/cfd2.mtx.gz | ./cg --seed 46"
+# command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
 #command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix {matrix}.mtx --seed {seed}"
 #command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
 
