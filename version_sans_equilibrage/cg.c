d9791b84f17b9a3ce2724903d7135af837b9319c
lolodino77 <laurentdangvu@gmail.com>
2020-06-04 14:40:16 +0200

    openmp vectorisation

--- version_sans_equilibrage/cg.c
+++ version_sans_equilibrage/cg.c
@@ -574,7 +574,7 @@ int main(int argc, char **argv)
 	// 	printf("\n");
 
 	// /*Algorithme du gradient conjugué */	
-	printf("r au debut :\n");
+	printf("r au debuts :\n");
 	if(my_rank == 5){
 		for (int i = 0; i < n; i++){	// r <-- r - alpha*q
 			printf("%lf ", r[i]);
