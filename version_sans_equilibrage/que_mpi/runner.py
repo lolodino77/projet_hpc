f874c6ba4fb55b691f0f44278f6b09e3a4173e56
DANG-VU <3800623@ppti-14-406-12.ufr-info-p6.jussieu.fr>
2020-06-05 23:36:40 +0200

    fin

--- version_sans_equilibrage/que_mpi/runner.py
+++ version_sans_equilibrage/que_mpi/runner.py
@@ -16,7 +16,7 @@ params['version'] = 3
 params['proof'] = 0x65dec1542f679f51
 
 # Paramètres du calcul (à adapter)
-params['matrix'] = "bcsstk13"
+params['matrix'] = "parabolic_fem"
 params['users'] = ["3800623"]
 
 # Description du code exécuté
@@ -37,7 +37,7 @@ Machines de bureau DELL équipés de CPU Intel i3 à 2 coeurs, 3Ghz, et 4Go de R
 #   On peut ajouter toutes les options qu'on veut, utiliser mpiexec, etc.
 # command_line = "./cg --matrix {matrix}.mtx --seed {seed}"
 #command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | ./cg --seed 46"
-command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | mpiexec --n 17 -hostfile hostfile --map-by ppr:1:node ./cg --seed 45"
+command_line = "zcat /Infos/lmd/2019/master/ue/MU4IN903-2020fev/{matrix}.mtx.gz | mpiexec --n 17 -hostfile hostfile --map-by node ./cg --seed 45"
 # command_line = "zcat matrices/{matrix}.mtx.gz | ./cg --seed {seed}"
 #command_line = "mpiexec --n {cores} --hostfile nodes.txt --display-map ./cg --matrix {matrix}.mtx --seed {seed}"
 #command_line = "mpiexec --n {nodes} -hostfile nodes.txt --map-by ppr:1:node ./cg --matrix {matrix}.mtx --seed {seed}"
@@ -86,6 +86,8 @@ def do_computation():
         print()
         print("---------------------------------------------------------------")
         result = subprocess.run(cmd, shell=True, check=True, stdout=subprocess.PIPE)
+        print("resultat dans runner : ")
+        print(result)
         print("---------------------------------------------------------------")
         print()
     except subprocess.CalledProcessError as e:
