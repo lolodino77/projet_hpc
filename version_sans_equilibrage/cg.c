c46552b943ebf54964abcbd8b8024e41a99e9f39
lolodino77 <laurentdangvu@gmail.com>
2020-06-05 17:45:16 +0200

    checkpoint

--- version_sans_equilibrage/cg.c
+++ version_sans_equilibrage/cg.c
@@ -582,19 +582,21 @@ int main(int argc, char **argv)
 		}
 	}
 	if(argc != 4){
-		if(strcmp(argv[3], "checkpoint") == 0){	    
-			printf("calcul depuis le debut\n");
-			for (int i = 0; i < n; i++)
-				x[i] = 0.0;
-			for (int i = 0; i < n; i++)	// r <-- b - Ax == b
-				r[i] = b[i];
-			for (int i = 0; i < n; i++)	// z <-- M^(-1).r
-				z[i] = r[i] / d[i];
-			for (int i = 0; i < n; i++)	// p <-- z
-				p[i] = z[i];
-			rz_part = dot_part(r, z, i_ini, n_part);
-			MPI_Allreduce(&rz_part, &rz, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);// rz = dot(r,z)
-		}
+		printf("calcul depuis le debut\n");
+		#pragma omp for simd
+		for (int i = 0; i < n; i++)
+			x[i] = 0.0;
+		#pragma omp for simd
+		for (int i = 0; i < n; i++)	// r <-- b - Ax == b
+			r[i] = b[i];
+		#pragma omp for simd
+		for (int i = 0; i < n; i++)	// z <-- M^(-1).r
+			z[i] = r[i] / d[i];
+		#pragma omp for simd
+		for (int i = 0; i < n; i++)	// p <-- z
+			p[i] = z[i];
+		rz_part = dot_part(r, z, i_ini, n_part);
+		MPI_Allreduce(&rz_part, &rz, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);// rz = dot(r,z)
 	}
 	// 	printf("verif2\n");
 	// }
